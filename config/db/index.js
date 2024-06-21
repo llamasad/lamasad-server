73424bb4aee50ee819fa5fe33c1284046a0fe9e3
datdo <datdo27120003@gmail.cco>
2024-06-21 12:22:57 +0700

    123

--- config/db/index.js
+++ config/db/index.js
@@ -1,7 +1,12 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('lamasad_db', 'postgres', 'emchua18', {
-    host: 'localhost',
-    dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USERNAME,
+    process.env.DATABASE_PASSWORD,
+    {
+        host: process.env.DATABASE_HOST || 'localhost',
+        dialect: 'postgres',
+    },
+);
 
 module.exports = sequelize;
